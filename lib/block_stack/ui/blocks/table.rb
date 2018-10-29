69f96894e52c8a7cbaa63b23e924a284946ea3a1
Brandon Black <d2sm10@hotmail.com>
2018-10-29 08:35:28 -0600

    Enhanced table block to better support associations.

--- lib/block_stack/ui/blocks/table.rb
+++ lib/block_stack/ui/blocks/table.rb
@@ -1,6 +1,30 @@
 module BlockStack
   class Table < Block
 
+    def self.headers(obj)
+      case obj
+      when Array
+        headers(obj.first)
+      when Hash
+        obj.keys
+      when BlockStack::Model
+        return obj.config.table_columns if obj.config.table_columns
+        associations = obj.associations.map(&:attribute)
+        obj._attrs.reject do |k, v|
+          associations.include?(k) && k != :id ||
+          k != :id && (v[:options][:singleton] ||
+          v[:options].include?(:dformed) && !v[:options][:dformed])
+        end.keys.hmap { |k| [k.to_s.gsub('_', ' ').title_case, k] }
+      when BBLib::Effortless
+        obj._attrs.reject do |k, v|
+          k != :id && (v[:options][:singleton] ||
+          v[:options].include?(:dformed) && !v[:options][:dformed])
+        end.keys.hmap { |k| [k.to_s.gsub('_', ' ').title_case, k] }
+      else
+        { 'ID': :object_id, 'Name': :to_s, 'Class': :class }
+      end
+    end
+
     def default_locals(custom = {})
       {
         data:        [],
